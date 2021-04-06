efe85b49ecb07dec5e5557b950c78725b3b9e66e
Joshua Caleb Reyes Rosa <joshuacalebreyes77@gmail.com>
2021-04-06 16:51:37 -0600

    ActivityLogin in progress

---
+++ app/src/main/java/app/modules/appmcdouglas/usermanagement/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package app.modules.appmcdouglas.usermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.modules.appmcdouglas.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/app/modules/appmcdouglas/usermanagement/LoginActivity.java
@@ -0,0 +1,16 @@
+package app.modules.appmcdouglas.usermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.modules.appmcdouglas.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
